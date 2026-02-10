33ed087c138421dcd307f424ee7afb07a73bde9f
trsyjr <trsyjr@dswd.gov.ph>
2026-02-10 20:21:13 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind v4 is working 🚀
+      </h1>
+    </div>
+  )
+}
+
